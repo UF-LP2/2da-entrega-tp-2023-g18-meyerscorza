ca46e0f722f42489c3d86a85d911fa3b5466deee
Martina-mey <matumeyer0109@gmail.com>
2023-11-07 10:15:55 -0300

    saco test desactualizados

--- test/test_Enfermero.py
+++
@@ -1,8 +0,0 @@
-from library.cEnfermero import cEnfermero
-from src.cPaciente import cPaciente
-
-def test_colorXsintomas():
-    paciente=cPaciente(1,"2006-12-21 01:33:46","Marcos","Lopez","paro_cardiaco","otalgias","tos")
-    enfermero=cEnfermero(3,"Luis","Armada")
-    enfermero.colorSintomaGrave(paciente)
-    assert enfermero.colorSintomaGrave() == "rojo"
--- test/test_hospital.py
+++
@@ -1,3 +0,0 @@
-def test_Cargar_Listas(): #hacerlo
-    pass
-    
